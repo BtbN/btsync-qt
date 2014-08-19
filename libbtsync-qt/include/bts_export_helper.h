3856853890bfb60f57b531d140f2f938ae46b827
BtbN <btbn@btbn.de>
2014-08-19 21:07:11 +0200

    Stuff

---
+++ libbtsync-qt/include/bts_export_helper.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef _WIN32
+# ifdef libbtsync-qt_EXPORTS
+#  define LIBBTS_EXPORT __declspec(dllexport)
+# else
+#  define LIBBTS_EXPORT __declspec(dllimport)
+# endif
+#else
+# define LIBBTS_EXPORT
+#endif
